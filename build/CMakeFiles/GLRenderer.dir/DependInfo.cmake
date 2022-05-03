fcd5a123bc375f97b29276094d039eddea951b31
JulianKoll <julian.kollenbroich@gmail.com>
2022-05-03 20:25:11 +0200

    cmake for laptop

--- build/CMakeFiles/GLRenderer.dir/DependInfo.cmake
+++ build/CMakeFiles/GLRenderer.dir/DependInfo.cmake
@@ -1,23 +1,32 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
   )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/jesus/vscode-workspace/GLRenderer/glad.c" "/home/jesus/vscode-workspace/GLRenderer/build/CMakeFiles/GLRenderer.dir/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/allah/Desktop/Vscode-Workspace/Cpp/GLRenderer/glad.c" "CMakeFiles/GLRenderer.dir/glad.c.o" "gcc" "CMakeFiles/GLRenderer.dir/glad.c.o.d"
-  "/home/allah/Desktop/Vscode-Workspace/Cpp/GLRenderer/Camera.cpp" "CMakeFiles/GLRenderer.dir/Camera.cpp.o" "gcc" "CMakeFiles/GLRenderer.dir/Camera.cpp.o.d"
-  "/home/allah/Desktop/Vscode-Workspace/Cpp/GLRenderer/Cube.cpp" "CMakeFiles/GLRenderer.dir/Cube.cpp.o" "gcc" "CMakeFiles/GLRenderer.dir/Cube.cpp.o.d"
-  "/home/allah/Desktop/Vscode-Workspace/Cpp/GLRenderer/Drawable.cpp" "CMakeFiles/GLRenderer.dir/Drawable.cpp.o" "gcc" "CMakeFiles/GLRenderer.dir/Drawable.cpp.o.d"
-  "/home/allah/Desktop/Vscode-Workspace/Cpp/GLRenderer/EventManager.cpp" "CMakeFiles/GLRenderer.dir/EventManager.cpp.o" "gcc" "CMakeFiles/GLRenderer.dir/EventManager.cpp.o.d"
-  "/home/allah/Desktop/Vscode-Workspace/Cpp/GLRenderer/Model.cpp" "CMakeFiles/GLRenderer.dir/Model.cpp.o" "gcc" "CMakeFiles/GLRenderer.dir/Model.cpp.o.d"
-  "/home/allah/Desktop/Vscode-Workspace/Cpp/GLRenderer/Renderer.cpp" "CMakeFiles/GLRenderer.dir/Renderer.cpp.o" "gcc" "CMakeFiles/GLRenderer.dir/Renderer.cpp.o.d"
-  "/home/allah/Desktop/Vscode-Workspace/Cpp/GLRenderer/Shader.cpp" "CMakeFiles/GLRenderer.dir/Shader.cpp.o" "gcc" "CMakeFiles/GLRenderer.dir/Shader.cpp.o.d"
-  "/home/allah/Desktop/Vscode-Workspace/Cpp/GLRenderer/Sphere.cpp" "CMakeFiles/GLRenderer.dir/Sphere.cpp.o" "gcc" "CMakeFiles/GLRenderer.dir/Sphere.cpp.o.d"
-  "/home/allah/Desktop/Vscode-Workspace/Cpp/GLRenderer/main.cpp" "CMakeFiles/GLRenderer.dir/main.cpp.o" "gcc" "CMakeFiles/GLRenderer.dir/main.cpp.o.d"
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jesus/vscode-workspace/GLRenderer/Camera.cpp" "/home/jesus/vscode-workspace/GLRenderer/build/CMakeFiles/GLRenderer.dir/Camera.cpp.o"
+  "/home/jesus/vscode-workspace/GLRenderer/Cube.cpp" "/home/jesus/vscode-workspace/GLRenderer/build/CMakeFiles/GLRenderer.dir/Cube.cpp.o"
+  "/home/jesus/vscode-workspace/GLRenderer/Drawable.cpp" "/home/jesus/vscode-workspace/GLRenderer/build/CMakeFiles/GLRenderer.dir/Drawable.cpp.o"
+  "/home/jesus/vscode-workspace/GLRenderer/EventManager.cpp" "/home/jesus/vscode-workspace/GLRenderer/build/CMakeFiles/GLRenderer.dir/EventManager.cpp.o"
+  "/home/jesus/vscode-workspace/GLRenderer/Model.cpp" "/home/jesus/vscode-workspace/GLRenderer/build/CMakeFiles/GLRenderer.dir/Model.cpp.o"
+  "/home/jesus/vscode-workspace/GLRenderer/Renderer.cpp" "/home/jesus/vscode-workspace/GLRenderer/build/CMakeFiles/GLRenderer.dir/Renderer.cpp.o"
+  "/home/jesus/vscode-workspace/GLRenderer/Shader.cpp" "/home/jesus/vscode-workspace/GLRenderer/build/CMakeFiles/GLRenderer.dir/Shader.cpp.o"
+  "/home/jesus/vscode-workspace/GLRenderer/Sphere.cpp" "/home/jesus/vscode-workspace/GLRenderer/build/CMakeFiles/GLRenderer.dir/Sphere.cpp.o"
+  "/home/jesus/vscode-workspace/GLRenderer/main.cpp" "/home/jesus/vscode-workspace/GLRenderer/build/CMakeFiles/GLRenderer.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
   )
 
 # Targets to which this target links.
